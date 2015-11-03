da0478061655e16ee82270796ad9c488b5e45ef2
Carlos Oliveira <carlos.baptista@b2wdigital.com>
2015-11-03 13:02:06 -0200

    feat(auto): Patch XHR auto

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import XHREvent from './xhrEvent.js';
 import XHRHelper from './xhrHelper';
 
-const xhrHelper = new XHRHelper(window, XMLHttpRequest);
-XHREvent.start(xhrHelper, XMLHttpRequest);
+if (window && XMLHttpRequest) {
+  const xhrHelper = new XHRHelper(window, XMLHttpRequest);
+  XHREvent.start(xhrHelper, XMLHttpRequest);
+}
 
 module.exports = {
   XHREvent
