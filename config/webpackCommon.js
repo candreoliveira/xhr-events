0e9fdfac432742161a4ec353463ade6374550a34
Carlos Oliveira <carlos.baptista@b2wdigital.com>
2015-11-03 14:31:30 -0200

    fix(prepublish): Run script before publishing it

--- config/webpackCommon.js
+++ config/webpackCommon.js
@@ -1,7 +1,7 @@
 var path = require('path');
 module.exports = {
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '..', 'dist'),
     filename: '[name].js'
   },
   module: {
