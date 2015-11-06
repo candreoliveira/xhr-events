a26cae2961429fe3dbe4610fc87404b01b837218
Carlos Oliveira <carlos.baptista@b2wdigital.com>
2015-11-06 20:07:22 -0200

    fix(xhrData)

--- src/xhrEvent.js
+++ src/xhrEvent.js
@@ -19,37 +19,37 @@ export default class XHREvent {
 
       this.onloadstart = (XMLHttpRequestEvent) => {
         xhrTiming.loadStartDate = new Date();
-        xhrData.dispatchNewMessage('XHRLoadStart', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRLoadStart', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       this.onloadend = (XMLHttpRequestEvent) => {
         xhrTiming.loadEndDate = new Date();
-        xhrData.dispatchNewMessage('XHRLoadEnd', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRLoadEnd', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       this.onload = (XMLHttpRequestEvent) => {
         xhrTiming.loadDate = new Date();
-        xhrData.dispatchNewMessage('XHRLoad', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRLoad', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       this.onabort = (XMLHttpRequestEvent) => {
         xhrTiming.abortDate = new Date();
-        xhrData.dispatchNewMessage('XHRAbort', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRAbort', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       this.ontimeout = (XMLHttpRequestEvent) => {
         xhrTiming.timeoutDate = new Date();
-        xhrData.dispatchNewMessage('XHRTimeout', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRTimeout', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       this.onerror = (XMLHttpRequestEvent) => {
         xhrTiming.errorDate = new Date();
-        xhrData.dispatchNewMessage('XHRError', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRError', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       this.onprogress = (XMLHttpRequestEvent) => {
         xhrTiming.progressDates = xhrTiming.progressDates.concat([new Date()]);
-        xhrData.dispatchNewMessage('XHRProgress', ObjectAssign({XMLHttpRequestEvent}, xhrTiming.dates()));
+        xhrData.dispatchNewMessage('XHRProgress', ObjectAssign({XMLHttpRequestEvent, request: xhrData.request}, xhrTiming.dates()));
       }
 
       XHROpen.apply(this, arguments);
